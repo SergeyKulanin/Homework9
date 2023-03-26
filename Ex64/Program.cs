3b8613dc4d3dcc09f45a8da160eb3f2052ff11e8
Sergey Kulanin <sjulanin1981@gmail.com>
2023-03-26 16:28:30 +0300

    вывести числа от M до N

---
+++ Ex64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int GetNumbersFromMToN(int number1, int number2)
+{
+    if(number1>number2) return number2;
+    
+    else
+    {
+        Console.Write(number1 + ", ");
+        number1++;
+    
+    }
+    
+    return GetNumbersFromMToN(number1++, number2);
+
+}
+
+Console.WriteLine("Enter M: ");
+int numStart = Convert.ToInt32(Console.ReadLine());;
+
+Console.WriteLine("Enter M: ");
+int numFinish = Convert.ToInt32(Console.ReadLine());
+
+GetNumbersFromMToN(numStart,numFinish);
