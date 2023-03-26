f9dc532e11ed8e39661074079f185e645a27cb98
Sergey Kulanin <sjulanin1981@gmail.com>
2023-03-26 16:39:18 +0300

    Найти сумму элементов

---
+++ Ex66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке 
+// от M до N с помощью рекурсии.
+
+Console.WriteLine("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void GetSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GetSum(numberM, numberN, sum);
+}
+
+GetSum(numberM, numberN, 0);
