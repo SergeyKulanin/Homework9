c4a10ddf1140764f1c793b4c42b804e5dfa73626
Sergey Kulanin <sjulanin1981@gmail.com>
2023-03-28 18:01:25 +0300

    Найти наибольший общий делитель

---
+++ Ex68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД)
+// этих чисел с помощью рекурсии.
+
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+FindGreatestCommonDivisor(M, N);
+
+void FindGreatestCommonDivisor(int M, int N)
+{
+ if (M > N) M = M - N;
+        if (M == 0)
+        {
+            Console.WriteLine($"Наибольший общий делитель равен {M}");
+            return;
+        } 
+        FindGreatestCommonDivisor(M, N);
+}
+
